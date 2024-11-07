b37d5b81268f85cd09aca07dfe9db896dfa287b3
ren-joey <tsuyiren@gmail.com>
2024-11-07 22:47:10 +0800

    feat: 🎸 JWT token development finished

--- src/main/java/example/security/JwtUtil.java
+++ src/main/java/example/security/JwtUtil.java
@@ -3,20 +3,33 @@ package example.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ClaimsBuilder;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JwtUtil {
-    private final SecretKey key = Keys.hmacShaKeyFor("your-secure-key".getBytes(StandardCharsets.UTF_8));
+    private final SecretKey key = Keys
+            .hmacShaKeyFor("your-very-secure-and-long-key-for-jwt-generation".getBytes(StandardCharsets.UTF_8));
+
+    public String extractUsername(String token) {
+        return extractUsername(token, extractAllClaims(token));
+    }
+
+    public String extractUsername(String token, Claims claims) {
+        return claims.getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractExpiration(token, extractAllClaims(token));
+    }
+
+    public Date extractExpiration(String token, Claims claims) {
+        return claims.getExpiration();
+    }
 
     public String generateToken(String username) {
         // 1 day in milliseconds
@@ -26,7 +39,7 @@ public class JwtUtil {
         claims.add("sub", username);
         claims.add("role", "user");
         claims.add("iat", new Date());
-        claims.add("exp", new Date(System.currentTimeMillis() + EXPIRATION_TIME));;
+        claims.add("exp", new Date(System.currentTimeMillis() + EXPIRATION_TIME));
 
         return Jwts.builder()
                 .claims()
@@ -36,17 +49,17 @@ public class JwtUtil {
                 .compact();
     }
 
-    public String extractUsername(String token) {
-        // TODO: Implement the method
-        // https://javadoc.io/doc/io.jsonwebtoken/jjwt-api/latest/index.html
-        Claims claims = Jwts.parser()
-            .verifyWith(key)
-            .build()
-            .parse(token)
-            .getPayload();
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean isTokenValid(String token) {
-        return extractUsername(token) != null;
+        Claims claims = extractAllClaims(token);
+        return extractUsername(token, claims) != null
+                && extractExpiration(token, claims).before(new Date());
     }
 }
